49df398c44e279cb2ded90e702cb3f96ec5b3901
Chetan <ckalia@uwaterloo.ca>
2023-08-07 02:55:24 -0400

    change form to controlled component and added reset button

--- src/components/workout-form.js
+++ src/components/workout-form.js
@@ -1,48 +1,70 @@
-import React from "react";
+import React, {useState} from "react";
 import "./workout-form.css";
 
 export default function WorkoutForm() {
 
+    const initialFormData = {
+        workoutName: "",
+        workoutType: "push",
+        workoutSets: "",
+        workoutReps: "",
+    }
+
+    const [formData, setFormData] = useState(initialFormData);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        const form = e.target;
-        const formData = new FormData(form);
-        const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson);
+        console.log(formData);
+    }
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value,
+        }) )
+    }
+
+    const resetForm = () => {
+        setFormData(initialFormData);
     }
     
     return (
     <div className = "container-fluid">
         <h3 className = "form-title">Add Workout</h3>
-        <form method="post" onSubmit={handleSubmit}>
+        <form id = "workout-form" method="post" onSubmit={handleSubmit}>
                 <div className = "form-group">
-                    <label className="label" htmlFor = "workout-name">Workout Name</label>
-                    <input id = "workout-name" className = "form-control" name = "workout-name" type = "text" defaultValue="Enter Name" />
+                    <label className="label" htmlFor = "workoutName">Workout Name</label>
+                    <input id = "workout-name" className = "form-control" name = "workoutName" value = {formData.workoutName} placeholder="Shoulder Press" type = "text" onChange={handleChange} />
                 </div>
                 <div className="workout-type">
                     <p className="label">Workout Type</p>
                     <div className="form-check form-check-inline">
-                        <input  id = 'push' className="form-check-input" type="radio" name="workout-type" value = "push" defaultChecked = {true} />
+                        <input  id = 'push' className="form-check-input" type="radio" name="workoutType" value = "push" checked = {formData.workoutType === "push"} onChange={handleChange} />
                         <label className="form-check-label" htmlFor="push">
                             Push Exercise
                         </label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input id = 'pull' className="form-check-input" type="radio" name="workout-type" value = "pull" />
+                        <input id = 'pull' className="form-check-input" type="radio" name="workoutType" value = "pull" checked = {formData.workoutType === "pull"} onChange={handleChange} />
                         <label className="form-check-label" htmlFor="pull">
                             Pull Exercise
                         </label>
                     </div>
                 </div>
                 <div className="form-group">
-                    <label className="label" htmlFor = "workout-sets">Sets</label>
-                    <input id = 'workout-sets' className="form-control" name = "workout-sets" type="number" defaultValue="0" min="0"/>
+                    <label className="label" htmlFor = "workoutSets">Sets</label>
+                    <input id = 'workout-sets' className="form-control" name = "workoutSets" type="number" value = {formData.workoutSets} placeholder="0" onChange={handleChange} min="0"/>
                 </div>
                 <div className="form-group">
-                    <label className="label" htmlFor = "workout-reps">Reps</label>
-                    <input id = 'workout-reps' className="form-control" name = "workout-reps" type="number" defaultValue="0" min="0"/>
+                    <label className="label" htmlFor = "workoutReps">Reps</label>
+                    <input id = 'workout-reps' className="form-control" name = "workoutReps" value = {formData.workoutReps} onChange={handleChange} placeholder="0" type="number" min="0"/>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <div className="button-container">
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type = "button" className="btn btn-secondary" onClick={resetForm}>Reset</button>
+                </div>
+                
         </form>
     </div>
     )
