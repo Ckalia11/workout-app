e2892f6a5e0d258c600385a76595c5c9c5cba5bc
Chetan <ckalia@uwaterloo.ca>
2023-08-07 23:03:36 -0400

    added dynamic search field

--- src/components/workout-form.js
+++ src/components/workout-form.js
@@ -3,8 +3,14 @@ import "./workout-form.css";
 
 export default function WorkoutForm() {
 
+    let workouts = [
+        "shoulder press",
+        "bench press",
+        "bicep curl",
+    ]
+
     const currDate = new Date();
-    const formattedDate = currDate.toISOString().split('T')[0]
+    const formattedDate = currDate.toISOString().split('T')[0];
 
     const initialFormData = {
         workoutName: "",
@@ -17,9 +23,25 @@ export default function WorkoutForm() {
     }
 
     const [formData, setFormData] = useState(initialFormData);
+    const [searchResults, setSearchResults] = useState([]);
+
+    const handleSearchChange = (e) => {
+        handleChange(e);
+        const input = e.target.value;
+        const search = input.trim().toLowerCase();
+        const res = workouts.filter(workout => workout.startsWith(search));
+        setSearchResults(res);
+    }
+
+    const handleSearchSelection = (e, result) => {
+        let name = "workoutName";
+        handleSetFormData(name, result);
+        setSearchResults([]);
+      };
 
     const resetForm = () => {
         setFormData(initialFormData);
+        setSearchResults([]);
     }
 
     const handleSubmit = (e) => {
@@ -30,19 +52,32 @@ export default function WorkoutForm() {
 
     const handleChange = (e) => {
         const {name, value} = e.target;
+        handleSetFormData(name, value);
+    }
+
+    const handleSetFormData = (name, value) => {
         setFormData(prevData => ({
             ...prevData,
             [name]: value,
-        }) )
+        }))
     }
     
     return (
     <div className = "container-fluid">
         <h3 className = "form-title">Add Workout</h3>
         <form id = "workout-form" method="post" onSubmit={handleSubmit}>
-                <div className = "form-group col-md-5">
+                <div className = "search-container form-group col-md-5">
                     <label className="label" htmlFor = "workoutName">Workout Name</label>
-                    <input id = "workout-name" className = "form-control" name = "workoutName" value = {formData.workoutName} placeholder="Shoulder Press" type = "text" onChange={handleChange} />
+                    <input id = "workout-name" className = "form-control" name = "workoutName" value = {formData.workoutName} placeholder="shoulder press" type = "text" onChange={handleSearchChange} onBlur={() => setSearchResults([])} />
+                    {searchResults.length > 0 && (
+                    <ul className="search-results">
+                        {searchResults.map((result, index) => (
+                            <li className = "clicked" key={index} onMouseDown={(e) => handleSearchSelection(e, result)}>
+                            {result}
+                            </li>
+                        ))}
+                    </ul>
+                    )}
                 </div>
                 <div className="form-group col-md-3">
                     <label className="label" htmlFor="workoutDate">Date</label>
